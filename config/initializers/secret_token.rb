aa4f0c6432feaca76037284e3e3b9ab2bd904aca
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2013-03-18 13:05:23 -0700

    App is setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TemplateApp::Application.config.secret_token = '8168cefccb5999b850c2a16a5356bc2d777685ceb893b3b3df9af2e50e7570c07b12279ee4d85e2574d9b12b30beeaa894fc1f4abe0a11c8d66b2350fa3a077a'
