af1aa5cee3bfaf5d90787d6903da4fa864773722
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2013-04-01 13:45:41 -0700

    changed routes and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 TemplateApp::Application.routes.draw do
-  get "static_pages/home"
+  root to: 'static_pages#home'
 
-  get "static_pages/help"
+  match '/help', to: 'static_pages#help'
 
-  get "static_pages/about"
+  match '/about', to: 'static_pages#about'
 
-  get "static_pages/contactus"
+  match '/contactus', to: 'static_pages#contactus'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,35 +3,35 @@ require 'spec_helper'
 describe "StaticPages" do
 	describe "Home page" do
 		it "should have the content 'Template App'" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_content('Template App')
 		end
 	end
 	
 	describe "Help page" do
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_content('Help')
 		end
 	end
 
 	describe "About page" do
 		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_content('About Us')
 		end
 	end
 
 	describe "Contact Us page" do
 		it "should have the content 'Contact Us'" do
-			visit '/static_pages/contactus'
+			visit contactus_path
 			page.should have_content('Contact Us')
 		end
 	end
 
 	describe "Contact Us homepage link" do
 		it "should link back to the homepage" do
-			visit '/static_pages/contactus'
+			visit contactus_path
 			page.should have_selector('a', href: '/static_pages/home')
 		end
 	end
