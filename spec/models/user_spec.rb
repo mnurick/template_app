ab4721ba1a832c4fffe7bd126ba2024ef581a57b
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2013-04-08 14:09:04 -0700

    added more user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,19 @@ describe User do |variable|
 
 	it { should respond_to(:name)}
 	it { should respond_to(:email)}
+
+	it { should be_valid}
+
+	describe "when name is blank" do
+		before { @user.name = " "}
+		it {should_not be_valid}
+	end
+	describe "when email is blank" do
+		before { @user.email = " "}
+		it {should_not be_valid}
+	end
+	describe "when name is too long" do
+		before { @user.name = "a" * 51}
+		it {should_not be_valid}
+	end
 end
