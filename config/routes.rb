e6b6cec3380e0bd08618c520dc205d141ac637b2
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2013-04-08 13:50:37 -0700

    added user model and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TemplateApp::Application.routes.draw do
+
   root to: 'static_pages#home'
 
   match '/help', to: 'static_pages#help'
@@ -6,6 +7,8 @@ TemplateApp::Application.routes.draw do
   match '/about', to: 'static_pages#about'
 
   match '/contactus', to: 'static_pages#contactus'
+
+  match '/signup', to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
